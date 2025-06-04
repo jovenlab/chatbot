35d4c37b53a9d8097b918c64d709661301d3862d
jovenlab <jolabsjunior@gmail.com>
2025-06-04 15:46:38 +0800

    Link removed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useEffect, useState } from 'react';
-import Link from 'next/link';
+// import Link from 'next/link';
 import Navbar from './components/Navbar';
 
 export default function HomePage() {
