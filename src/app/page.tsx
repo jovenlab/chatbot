759a0fbccf588d73c63842b61bf172cf905aa40f
jovenlab <jolabsjunior@gmail.com>
2025-06-04 13:19:22 +0800

    api/hello

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   const [message, setMessage] = useState("");
 
   useEffect(() => {
-    fetch("https://chatbot-backend-wipk.onrender.com/")
+    fetch("https://chatbot-backend-wipk.onrender.com/api/hello")
       .then(res => res.json())
       .then(data => setMessage(data.message));
   }, []);
